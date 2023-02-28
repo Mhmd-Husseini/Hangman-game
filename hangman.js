bfc90f4a63ff9e1071cea807fee02f6410d927a0
Husseini <mhmd.husseinyy@gmail.com>
2023-02-28 05:52:58 +0200

    Defining functions guessletter and checkgameover (JS)

--- hangman.js
+++ hangman.js
@@ -12,4 +12,25 @@ const jguessedLetters = document.getElementById("guessed-letters");
 const jgameOver = document.getElementById("game-over");
 const jguessInput = document.getElementById("guess-input");
 const jguessButton = document.getElementById("guess-button");
+const jresult = document.getElementById("result");
 
+function guessLetter(letter) {
+  if (!word.includes(letter)) {
+    remainingGuesses--;
+  } else
+	guessedLetters[letter] = true;
+}
+
+function checkGameOver() {
+  if (remainingGuesses <= 0) {
+    jresult.innerhtml="Sorry, You Lost"
+    return true;
+  }
+  for (const letter of word) {
+    if (!guessedLetters[letter]) {
+      return false;
+    }
+  }
+  jresult.innerhtml="WOHOOO, You Won!!!"
+  return true;
+}
